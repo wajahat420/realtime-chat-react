4feeda98e881231438c93aed0a7e7b68f727ba12
Wajahat Sarwat <42533972+wajahat420@users.noreply.github.com>
2022-01-24 07:15:00 +0500

    working

--- src/components/body/index.jsx
+++ src/components/body/index.jsx
@@ -18,20 +18,23 @@ const Body = ({activeChat, realTimeMsg}) => {
     abc = textMessage
     let messageBox = useRef()
 
-    let loggedUserdID = "999"
+    let loggedUserdID = localStorage.getItem("id")
 
     const [user, setUser] = useState({})
+
+    const [block, setBlock] = useState(false)
+
     dupUser = user
 
     const [message, setMessage] = useState(
         [
-            {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
-            {sender:true, type:"image", message:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum nesciunt alias incidunt, quia quod ad laborum perferendis rerum recusandae illum provident suscipit corporis, obcaecati vel. Id laudantium totam dolore. Ratione minus dolorem dolorum magnam exercitationem similique quisquam magni vel expedita, quibusdam quidem quis rem voluptatum sed asperiores minima laborum. Consequuntur, illo voluptas! Ad ratione provident quis aut molestias excepturi suscipit laborum laudantium, quia quisquam in asperiores itaque molestiae assumenda minima libero blanditiis quo fugiat maxime a impedit? Quasi quaerat vel est recusandae maiores. Impedit suscipit aperiam nesciunt blanditiis. Iusto doloribus dicta porro nesciunt adipisci. Esse perferendis nobis reprehenderit ducimus!", userID:'1', receiverID:'2'},
-            {sender:true, type:"doc", message:"first", userID:'1', receiverID:'2'},
-            {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
-            {sender:false, type:"text", message:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum nesciunt alias incidunt, quia quod ad laborum perferendis rerum recusandae illum provident suscipit corporis, obcaecati vel. Id laudantium totam dolore. Ratione minus dolorem dolorum magnam exercitationem similique quisquam magni vel expedita, quibusdam quidem quis rem voluptatum sed asperiores minima laborum. Consequuntur, illo voluptas! Ad ratione provident quis aut molestias excepturi suscipit laborum laudantium, quia quisquam in asperiores itaque molestiae assumenda minima libero blanditiis quo fugiat maxime a impedit? Quasi quaerat vel est recusandae maiores. Impedit suscipit aperiam nesciunt blanditiis. Iusto doloribus dicta porro nesciunt adipisci. Esse perferendis nobis reprehenderit ducimus!", userID:'1', receiverID:'2'},
-            {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
-            {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
+            // {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
+            // {sender:true, type:"image", message:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum nesciunt alias incidunt, quia quod ad laborum perferendis rerum recusandae illum provident suscipit corporis, obcaecati vel. Id laudantium totam dolore. Ratione minus dolorem dolorum magnam exercitationem similique quisquam magni vel expedita, quibusdam quidem quis rem voluptatum sed asperiores minima laborum. Consequuntur, illo voluptas! Ad ratione provident quis aut molestias excepturi suscipit laborum laudantium, quia quisquam in asperiores itaque molestiae assumenda minima libero blanditiis quo fugiat maxime a impedit? Quasi quaerat vel est recusandae maiores. Impedit suscipit aperiam nesciunt blanditiis. Iusto doloribus dicta porro nesciunt adipisci. Esse perferendis nobis reprehenderit ducimus!", userID:'1', receiverID:'2'},
+            // {sender:true, type:"doc", message:"first", userID:'1', receiverID:'2'},
+            // {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
+            // {sender:false, type:"text", message:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum nesciunt alias incidunt, quia quod ad laborum perferendis rerum recusandae illum provident suscipit corporis, obcaecati vel. Id laudantium totam dolore. Ratione minus dolorem dolorum magnam exercitationem similique quisquam magni vel expedita, quibusdam quidem quis rem voluptatum sed asperiores minima laborum. Consequuntur, illo voluptas! Ad ratione provident quis aut molestias excepturi suscipit laborum laudantium, quia quisquam in asperiores itaque molestiae assumenda minima libero blanditiis quo fugiat maxime a impedit? Quasi quaerat vel est recusandae maiores. Impedit suscipit aperiam nesciunt blanditiis. Iusto doloribus dicta porro nesciunt adipisci. Esse perferendis nobis reprehenderit ducimus!", userID:'1', receiverID:'2'},
+            // {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
+            // {sender:true, type:"text", message:"first", userID:'1', receiverID:'2'},
         ])
 
     const handleFile = (e) => {
@@ -43,47 +46,44 @@ const Body = ({activeChat, realTimeMsg}) => {
     }
 
     const sendMsg = () => {
-        console.log("SENDING");
 
         axios.post(`${REACT_APP_API_URL}/sendMessage`, {
-            // type: "text",
-            // message : textMessage,
-            senderID : "123",
-            receiverID : "999"
+            type: "text",
+            message : textMessage,
+            senderID : loggedUserdID,
+            receiverID : user.id
         })
         .then(res => console.log("RESS", res))
         .catch(err => console.log("ERR", err))
+
     }
 
-    const checkReceiverActiveChat = (data) =>{
-        // console.log("receiverActiveChat", data);
-        console.log("USERR", dupUser);
-
-        
-        if(loggedUserdID === data.receiverID){
-            axios.post(`${REACT_APP_API_URL}/sendMessageChecking`, {
-                    checked : data.senderID === dupUser.id,
-                    senderID: data.senderID,
-                    receiverID : data.receiverID
-            })
-            .then(res => console.log("sendMessageChecking", res))
-            .catch(err => console.log("ERR", err))
-            // sendCurrentChatID()
-        }
-    }
-
-    const setToDB = (checked) => {
-
-        axios.post(`${REACT_APP_API_URL}/sendMessageToDB`, {
-            type: "text",
-            message : abc,
-            senderID : "999",
-            receiverID : "123",
-            checked : checked
+    const loadData = () => {
+        axios.post(`${REACT_APP_API_URL}/getChat`, {
+            receiverID : activeChat.id,
+            senderID : loggedUserdID
         })
-        .then(res => console.log("RES", res))
-        .catch(err => console.log("ERR", err))
+        .then(res => {
+            console.log("LOAD DATA", res.data);
+            setBlock(res.data.block)
+            setMessage(res.data.findChat?.messages || [])
+        })
+        .catch(err => console.log("err", err))
+
     }
+
+    // const setToDB = (data) => {
+
+    //     axios.post(`${REACT_APP_API_URL}/sendMessageToDB`, {
+    //         type: "text",
+    //         message : abc,
+    //         senderID : data.senderID,
+    //         receiverID : data.receiverID,
+    //         checked : data.checked
+    //     })
+    //     .then(res => console.log("RES", res))
+    //     .catch(err => console.log("ERR", err))
+    // }
 
     useEffect(() => {
         if (messageBox) {
@@ -92,29 +92,26 @@ const Body = ({activeChat, realTimeMsg}) => {
             target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
           });
         }
-
-
-        socket.on("receiverActiveChat", (data) => {
-            checkReceiverActiveChat(data)
-        })
-
-        socket.on('receiverActiveChatResponse', (data) => {
-            setToDB(data.checked)
-        })
       }, [])
 
     useEffect(() => {
+        loadData()
         setUser(activeChat)
+        setMessage([])
     }, [activeChat])  
     
     useEffect(() => {
-        if(realTimeMsg.senderID == user.id){
+        // console.log("Realtime msg in body", realTimeMsg);
+        // console.log("user in body", user);
+        if(realTimeMsg.senderID == user.id || realTimeMsg.senderID === loggedUserdID){
+            console.log("IF");
             setMessage([
+                // ...message,
                 ...message,
                 realTimeMsg
             ])
         }
-    }, [realTimeMsg.message])   
+    }, [realTimeMsg])   
 
     // console.log("realTimeMsg",textMessage);
 
@@ -124,21 +121,27 @@ const Body = ({activeChat, realTimeMsg}) => {
             <div className="body_top">
                 <div className="body_topPerson">
                     <div className="body_topLeft">
-                        <img src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80' alt="" />
+                        <img src={user.image} alt="" />
                     </div>
                     <div className="body_topRight">
-                        <p className="body_topName">Yogi Yogi Yogi Yogi Yogi Yogi</p>
+                        <p className="body_topName">{user.name}</p>
                         <p className="body_topMsg">{activeChat.status}</p>
                     </div>
                 </div>
                 <div className="body_block">
-                    <img src={ImgBlock} alt="" />
+                    {
+                        block
+                        ?
+                        <img src={ImgBlock} alt="" />
+                        :
+                        <img style={{width:25,height:25,marginRight:10}} src={"https://w7.pngwing.com/pngs/741/68/png-transparent-user-computer-icons-user-miscellaneous-cdr-rectangle-thumbnail.png"} alt="" />
+                    }
                 </div>
             </div>
             <div className="body_main">
                 {
                     message.map((elem,index) => (
-                        elem.sender
+                        elem.senderID === loggedUserdID
                         ?
                         <div className="body_sender">
                             {/* <div className="body_senderLeft">
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -7,32 +7,70 @@ import axios from 'axios';
 
 const REACT_APP_API_URL = "http://localhost:5000"
 
+ let val = ''
 
 const Main = () => {
 
-    const loggedUserdID = "1"
+    let loggedUserdID = localStorage.getItem("id")
+
     const [activeChat, setActiveChat] = useState({})
     const [realTimeMsg, setRealTimeMsg] = useState({})
+    
+    // const [activeChat, setActiveChat] = useState({})
+
+    const checkReceiverActiveChat = (data) =>{
+        
+        // if(loggedUserdID === data.receiverID){
+            // console.log("IS RECEIVER", data);
+            setRealTimeMsg(data)
+            // axios.post(`${REACT_APP_API_URL}/sendMessageChecking`, {
+            //         checked : data.senderID === dupUser.id,
+            //         senderID: data.senderID,
+            //         receiverID : data.receiverID
+            // })
+            // .then(res => console.log("sendMessageChecking", res))
+            // .catch(err => console.log("ERR", err))
+        // }
+    }
 
     useEffect(()=>{
-        // const socket = socketIOClient(process.env.REACT_APP_API_URL);
-        // socket.on("messages", (data) => setLastMessage(data));
         const socket = socketIOClient(REACT_APP_API_URL);
-        socket.emit("connected", loggedUserdID)
+        socket.emit("addUser", loggedUserdID)
 
-        socket.on("message", (data) => setRealTimeMsg(data));
+        // socket.on("message", (data) =>   setRealTimeMsg(data));
+
+        // socket.emit("addUser", ())
 
         socket.on("getUsers", (data) => {
-            const find = data.findIndex(elem => elem.userId == activeChat.id)
-            console.log("find", find, data);
-            console.log("activeChat", activeChat);
-            if(find === -1){
-                setActiveChat({...activeChat, status : "offline"})
-            }else{
-                setActiveChat({...activeChat, status : "online"})
-            }
+            console.log(
+                "USERS GET",
+                data
+            );
+            // const find = data.findIndex(elem => elem.userId == activeChat.id)
+            // console.log("find", find, data);
+            // console.log("activeChat", activeChat);
+            // if(find === -1){
+            //     setActiveChat({...activeChat, status : "offline"})
+            // }else{
+            //     setActiveChat({...activeChat, status : "online"})
+            // }
         });
 
+        socket.on("showMessage", (data) => {
+
+            if(loggedUserdID === data.receiverID){
+                console.log("SHOWMSG==", data);
+            }
+            // setRealTimeMsg(data)
+        })
+
+        socket.on("receiverActiveChat", (data) => {
+            checkReceiverActiveChat(data)
+        })
+        
+        return () => {
+            alert("unmount")
+        }
         
     }, [])
 
@@ -44,12 +82,21 @@ const Main = () => {
         .catch(err => console.log("ERR", err))
     }
 
+    const changeVal = async(e) => {
+        // alert("a")
+        await localStorage.setItem('id', e.target.value)
+    }
+    const get = localStorage.getItem('id')
+    // console.log("GET", get);
+
     return(
         <div className="container">
+            <h2>{get}</h2>
+            <input  type='text' onBlur={changeVal} onChange={e => val =  e.target.value}/>
             <div className="main">
                 <div className="box">
                     <div className="main_left">
-                        <Contacts setChat={(user) => setActiveChat(user)}/>
+                        <Contacts realTimeMsg={realTimeMsg} setChat={(user) => setActiveChat(user)}/>
                     </div>
                     <div className="main_right">
                         <Body realTimeMsg={realTimeMsg} activeChat={activeChat}/>
